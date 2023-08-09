80ac1a004360472e5eb3316fc437062ded787ca8
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-08-09 20:47:27 -0300

    updating variables.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,8 @@ resource "google_artifact_registry_repository" "example_app_repo" {
 }
 
 resource "google_storage_bucket" "terraform_state_bucket" {
-  name          = var.gcs_bucket_name
-  location      = var.gcs_bucket_location
+  name          = var.bucket_name
+  location      = var.bucket_location
   storage_class = "STANDARD"
   force_destroy = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,13 @@ variable "repository_name" {
   type        = string
   description = "this variable contains the repository name"
 }
+
+variable "bucket_name" {
+  type        = string
+  description = "this variable contains the tfstate gcs bucket id"
+}
+
+variable "bucket_location" {
+  type        = string
+  description = "this variable contains the location of the tfstate gcs bucket"
+}
